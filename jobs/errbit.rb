4207f145e14342cf6a901a68d80c0234ad53d8d6
Anay Nayak <anayak@thoughtworks.com>
2014-10-26 11:32:36 +0530

    Using /problems endpoint for errbit since stats isnt available with current version

--- jobs/errbit.rb
+++ jobs/errbit.rb
@@ -3,14 +3,6 @@ config = YAML.load File.open("config.yml")
 config = config[:errbit]
 
 SCHEDULER.every "5m", first_in: 0 do |job|
-
-  projects = config[:projects].each_with_object([]) do |data, projects|
-    project_name, project_attributes = data
-    options = project_attributes.merge(date_format: config[:date_format], base_uri: config[:base_uri])
-
-    projects << Errbit.new(options).values
-  end
-
-  send_event "errbit", { projects: projects }
-
+  projects = Errbit.new(config).projects
+  send_event "errbit", { projects: projects}
 end
--- lib/errbit.rb
+++ lib/errbit.rb
@@ -1,50 +1,100 @@
-require "open-uri"
-require "json"
+require "httparty"
 
 class Errbit
 
-  attr_reader :api_key, :base_uri, :date_format, :response
+  attr_reader :api_key, :base_uri, :response
 
   def initialize(options)
     @api_key     = options[:api_key]
     @base_uri    = options[:base_uri]
-    @date_format = options[:date_format]
-    @response    = parse URI.parse(errbit_path).read
+    @response    = HTTParty.get(errbit_path).parsed_response
   end
 
-  def values
-    {
-      name:    name,
-      status:  status,
-      date:    date,
-      errors:  errors
-    }
+  def projects
+    ErrbitErrors.from_response(response).per_app.map do |app, errors|
+      {
+        name: app,
+        errors: errors.unresolved.size,
+        date: errors.last_date,
+        status: errors.unresolved.size == 0 ? "green" : "red"
+      }
+    end
   end
 
   private
 
-  def name
-    response[:name]
-  end
-
-  def status
-    errors == 0 ? "green" : "red"
-  end
-
-  def date
-    DateTime.parse(response[:last_error_time]).strftime date_format if response[:last_error_time]
-  end
-
-  def errors
-    response[:unresolved_errors]
-  end
-
-  def parse(data)
-    JSON.parse data, symbolize_names: true
-  end
-
   def errbit_path
-    "#{base_uri}/api/v1/stats/app?api_key=#{api_key}"
+    "#{base_uri}/api/v1/problems?auth_token=#{api_key}&start_date=00:00&end_date=23:59"
   end
 
+  class ErrbitErrors
+    attr_reader :records
+    def initialize(records)
+      @records = records
+    end
+
+    def resolved
+      records.select {|r| r.resolved}
+    end
+
+    def unresolved
+      records - resolved
+    end
+
+    def self.from_response(response)
+      records = response.collect {|r|
+        ErrbitRecord.new(r)
+      }
+      ErrbitErrors.new(records)
+    end
+
+    def last_date
+      records.sort_by {|x| x.date}.first.date
+    end
+
+    def per_app
+      errors = records.group_by { |v| v.app}.map {|k, v|
+        [k, ErrbitErrors.new(v)]
+      }.flatten
+      Hash[*errors]
+    end
+
+  end
+
+  class ErrbitRecord
+    attr_reader :record
+    def initialize(record)
+      @record = record
+    end
+
+    def app
+      "#{record['app_name']} #{record['environment']}"
+    end
+
+    def where
+      record["where"]
+    end
+
+    def resolved
+      record["resolved"]
+    end
+
+    def message
+      record["message"]
+    end
+
+    def date
+      Time.parse(record["last_notice_at"]).utc
+    end
+
+    def to_hash
+      {
+        app: app,
+        where: where,
+        resolved: resolved,
+        message: message,
+        date: date
+      }
+    end
+  end
 end
